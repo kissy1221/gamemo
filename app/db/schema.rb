eb6c45eb2aa380a1fe1eaf1f2c568b069c8a0121
yuto <yuutomain20@gmail.com>
2025-11-18 18:26:31 +0900

    Userモデルのemailカラムをユニーク、NOT NULLに変更

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_18_043818) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_18_091330) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
