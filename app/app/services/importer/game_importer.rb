ca059150f7c52bfbf0fde4b06e7597a4b0637327
yuto <yuutomain20@gmail.com>
2025-12-30 13:59:58 +0900

    リファクタリング

--- app/app/services/importer/game_importer.rb
+++ app/app/services/importer/game_importer.rb
@@ -1,18 +1,40 @@
 class Importer::GameImporter
+  JAPAN_REGION_ID = 3
+  OFFICIAL_WEBSITE_TYPE = 1
+
+
   def initialize(api_data)
     @api_data = api_data
   end
 
   def import
-    game = Game.find_or_initialize_by(igdb_id: @api_data['id'])
-    japanese_game_localization = @api_data['game_localizations']&.find{|gl| gl['region'] == 3}
-    image_id = japanese_game_localization&.[]('cover')&.[]('image_id')
-    official_website = @api_data['websites']&.find{|w| w['type'] == 1}
+    game = Game.find_or_initialize_by(igdb_id: api_data['id'])
 
-    game.name = japanese_game_localization&.[]('name') || @api_data['name']
+    game.name = japanese_game_localization&.[]('name') || api_data['name']
     game.official_site_url = official_website&.[]('url')
-    game.cover_image_url = image_id.nil? ? nil : ExternalApi::Igdb::Constraints::COVER_IMAGE_URL_PREFIX + image_id
-    game.igdb_id = @api_data['id'] if game.new_record?
+    game.cover_image_url = cover_image_url
+    game.igdb_id = api_data['id'] if game.new_record?
     game.save!
   end
+
+  private
+  attr_reader :api_data
+
+  def japanese_game_localization
+    api_data['game_localizations']&.find{|gl| gl['region'] == JAPAN_REGION_ID}
+  end
+
+  def official_website
+    api_data['websites']&.find{|w| w['type'] == OFFICIAL_WEBSITE_TYPE}
+  end
+
+  def image_id
+    japanese_game_localization&.dig('cover','image_id')
+  end
+
+  def cover_image_url
+    return nil if image_id.nil?
+    ExternalApi::Igdb::Constraints::COVER_IMAGE_URL_PREFIX + image_id
+  end
+
 end
--- app/lib/tasks/import_games.rake
+++ app/lib/tasks/import_games.rake
@@ -2,14 +2,19 @@ namespace :igdb do
   task import_games: :environment do
     puts "Importing games..."
     offset = 0
+    total_imported = 0
+    total_failed = 0
     game_fetcher = ExternalApi::Igdb::GameFetcher.new
     loop do
       games = game_fetcher.fetch(offset)
       break if games.empty?
       games.each do |game|
         Importer::GameImporter.new(game).import
+        total_imported += 1
       end
-      offset = offset + ExternalApi::Igdb::Constraints::MAX_LIMIT
+      offset += ExternalApi::Igdb::Constraints::MAX_LIMIT
+      print "\rProgress: #{total_imported} games imported (offset: #{offset})"
+      $stdout.flush
     end
     puts "games imported successfully"
   end
